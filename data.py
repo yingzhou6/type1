b816653acb719146f400affd48750ff634cda6d8
yingzhou6 <110857859+yingzhou6@users.noreply.github.com>
2022-08-25 23:27:15 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime,timedelta
+import dash
+from dash import dcc
+from dash import html
+import plotly.graph_objs as go
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+import plotly.express as px
+
+sdg = pd.read_csv('sdg_12_tickers.csv')
+esg = pd.read_csv('esg_12_tickers.csv')
+sent = pd.read_csv('sentiment_12_tickers.csv')
+
+ticker = sdg['Ticker'].unique()
+ticker1 = esg['Ticker'].unique()
+ticker2 = sent['Ticker'].unique()
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUMEN],
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}])
+
+server = app.server
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1("ESG,SDG & Sentiment",
+                        className='text-center text-primary, mb-3'),
+                width=16)
+    ]),
+    dbc.Row([
+    dbc.Col(html.H1("SDG",
+                        className='text-center text-primary, mb-3'),
+                width=16)
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.P('Select Stock:', style={'textDecoration': 'underline'}),
+            dcc.Dropdown(id='my-company', multi=True, value=ticker,
+                         options=[{'label': x, 'value': x}
+                                  for x in ticker]),
+            dcc.Graph(id='fig1', figure={})
+        ], width={'size': 20}),
+    ]),
+    dbc.Row([
+        dbc.Col(html.H1("ESG",
+                        className='text-center text-primary, mb-3'),
+                width=16)
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.P('Select Date:', style={'textDecoration': 'underline'}),
+            dcc.Dropdown(id='my-date', multi=True, value=ticker1,
+                            options=[{'label': x, 'value': x}
+                                    for x in ticker1]),
+            dcc.Graph(id='fig2', figure={})
+        ], width={'size': 20}),
+    ]),
+    dbc.Row([
+        dbc.Col(html.H1("Sentiment",
+                        className='text-center text-primary, mb-3'),
+                width=16)
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.P('Select Date:', style={'textDecoration': 'underline'}),
+            dcc.Dropdown(id='my-sent', multi=True, value=ticker2,
+                            options=[{'label': x, 'value': x}
+                                    for x in ticker2]),
+            dcc.Graph(id='fig3', figure={})
+        ], width={'size': 20}),
+
+        ])
+    ])
+
+
+@app.callback(
+    Output('fig1', 'figure'),
+    Input('my-company', 'value')
+)
+def update_graph(com):
+    dff = sdg[sdg['Ticker'].isin(com)]
+    fighist = px.line(dff, x = 'Timestamp',y = 'SDG_Mean',color = 'Ticker')
+    return fighist
+
+
+@app.callback(
+    Output('fig2', 'figure'),
+    Input('my-date', 'value')
+)
+def update_graph(com):
+    dff = esg[esg['Ticker'].isin(com)]
+    fighist = px.line(dff, x = 'Timestamp',y = 'ESG_Mean',color = 'Ticker')
+    return fighist
+
+
+@app.callback(
+    Output('fig3', 'figure'),
+    Input('my-sent', 'value')
+)
+def update_graph(com):
+    dff = sent[sent['Ticker'].isin(com)]
+    fighist = px.line(dff, x = 'Timestamp',y = 'Sentiment',color = 'Ticker')
+    return fighist
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8000)
